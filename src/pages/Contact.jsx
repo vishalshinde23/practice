1b33407018416c96e903003371b952012bf5eca5
vishal <vishalms2342@gmail.com>
2025-09-19 12:27:41 +0530

    feat/add/new

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,12 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="text-center py-20 text-2xl font-semibold">
-      Contact Us Page
+      Contact Us Pageputvoruhhrr euvoirw
+      reytvthert
+      etytorwutrtrh
+      evwytertrrthwiet8tvb;ovwuty
+      rivty
+      rvuqwyt <u>vtotiuy</u>
     </div>
   );
 };
